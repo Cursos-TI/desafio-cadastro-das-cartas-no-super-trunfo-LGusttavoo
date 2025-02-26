6a5a561a8957eb0fceb6c852830766202861b21a
LGusttavoo <140545765+LGusttavoo@users.noreply.github.com>
2025-02-25 23:44:22 -0300

    Update CartasSuperTrunfo.c
    por enquanto. irei corrigir

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,39 +4,24 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    char estado; 
-    char carta;
-    char cidade[50];
-    int populacao;
-    float area;
-    float PIB;
-    int turismo;
+    char estado ='A'; 
+    char codigo = 'A01';
+    char cidade[50] = 'Fortaleza';
+    int populacao = 1890000;
+    float area = 3250.13;
+    float PIB = 800.20;
+    int turismo = 123;
 
-    printf("Digite seu estado:\n");
-    scanf("%s",&estado);
+    printf("Estado: %s/n",estado);
+    printf("Código: %s/n",codigo);
+    printf("Nome da Cidade: %s/n",cidade);
+    printf("População: %d/n",populacao);
+    printf("Área: %f km²\n",area);
+    printf("PIB: %f bilhões de reais/n",PIB);
+    printf("N'numero de Pontos Turisticos: %d/n",turismo);
     
-    printf("Código da carta:\n");
-    scanf("%s",&carta);
-
-    printf("Nome da Cidade:\n");
-    scanf("%s", &cidade);
-
-    printf("População:\n");
-    scanf("%d", &populacao);
-
-    printf("Área:\n");
-    scanf("%f" ,&Area);
-    
-    printf("Qual o PIB da cidade:\n");
-    scanf("%f" , &PIB);
-
-    printf("Numero de pontos Turisticos:\n");
-    scanf("%d", &turismo);
-
-    
-   
-    return 0;
 }
+
+  
